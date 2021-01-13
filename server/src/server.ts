bb048e3e79246eb1b2bad6a8a5172e89c43b3416
Nicoly de Oliveira <nicoly.oavelino2015@gmail.com>
2021-01-13 11:14:55 -0300

    + alterações

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 // Rota: endereço completo da requisição
@@ -21,5 +22,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
