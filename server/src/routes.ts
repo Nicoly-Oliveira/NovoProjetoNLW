3ddb7e54063df2b60f8dd548f97b9baaa18a57c0
Nicoly de Oliveira <nicoly.oavelino2015@gmail.com>
2021-01-15 13:22:57 -0300

    + alterações

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,45 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
+routes.get('/items', async (request, response) => {
     // JSON 
-    return response.json({ message: "Hello World"});
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            id:item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+
+    return response.json(serializedItems);
 });
 
+routes.post('/points', async (request, response) =>{
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+    return response.json({ success: true });
+});
 export default routes;
